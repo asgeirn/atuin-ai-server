69e2a08f3796c8f7d5a114a1209dee8d63477348
Michelle Tilley <michelle@michelletilley.net>
2026-08-10 11:57:58 -0700

    Bump atuin_ai_core to v0.3.0

--- mix.exs
+++ mix.exs
@@ -155,7 +155,7 @@ defmodule AtuinAI.Server.MixProject do
     [
       {:atuin_ai_core,
        git: "https://github.com/atuinsh/atuin-ai-core.git",
-       tag: "v0.2.5",
+       tag: "v0.3.0",
        compile: false,
        app: false},
       {:bandit, "~> 1.5"},
